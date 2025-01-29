87ed0ee6561eaad7661b5695fc908ff4806c8579
Nicolas Marquez <nicolas.marquezlima@gmail.com>
2025-01-29 13:28:14 -0300

    modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -4,15 +4,18 @@ limpar();
 function adicionar () {
     //recuperar valores nome do produto, valor e quantidade
     let produto = document.getElementById ('produto').value;
+    let quantidade = document.getElementById ('quantidade').value;
+
     let nomeProduto = produto.split('-')[0];
     let valorUnidade = produto.split('R$')[1];
-    let quantidade = document.getElementById ('quantidade').value;
     let preco = quantidade * valorUnidade;
+
     //listar produtos selecionados no carrinho de compras
     let listaProdutos = document.getElementById ('lista-produtos');
     listaProdutos.innerHTML = listaProdutos.innerHTML + `<section class="carrinho__produtos__produto">
           <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
         </section>`;
+        
     //calcular valor total da compra
     valorTotal = valorTotal + preco;
     let precoTotal = document.getElementById('valor-total');
